b800d873d11f69ad86f8c9bf52724bf0609a0edb
UncannyPronk <awesomespeedcuber@gmail.com>
2022-12-03 18:18:09 +0530

    main

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ def write(blit=True, text='sample text', position=(0, 0), color=(0, 0, 0), fonts
         return text, position
 
 class Node:
-    def __init__(self):
-        self.rect = Rect(0, 0, 200, 100)
+    def __init__(self, x, no=3):
+        self.rect = Rect(x*300 + 20, 0, 200, 100)
         self.name = "New Node"
-        self.inputno = 3
+        self.inputno = no
+        if self.inputno > 3:
+            self.inputno = 3
         self.anchor = False
         self.selected = False
         self.editedname = False
@@ -60,15 +62,27 @@ class Node:
             self.inrects[i][0].y = self.rect.y - 20 + 30*(i+1)
         return result
 
-node1 = Node()
-node2 = Node()
+nodes = []
+for i in range(2):
+    nodes.append(Node(i))
+nodes.append(Node(2, 2))
+nodes.append(Node(3, 1))
+nodes.append(Node(4, 0))
 connections = []
 
 def node_graph():
     running = True
-    hover1 = False
     key_time = 0
     while running:
+        screen.fill((0, 25, 20))
+
+        # Node follows mouse when selected
+        hover = []
+        for node in nodes:
+            hover.append(node.display())
+            if node.anchor:
+                node.rect.centerx, node.rect.centery = pygame.mouse.get_pos()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -79,87 +93,49 @@ def node_graph():
                     running = False
                     pygame.quit()
                     sys.exit()
-                if node1.selected:
-                    if not node1.editedname:
-                        node1.name = ""
-                        node1.editedname = True
-                    if event.key == K_DELETE:
-                        node1.name = ""
-                    if event.key == K_BACKSPACE:
-                        node1.name = node1.name[:-1]
-                    elif event.key == K_MINUS:
-                        node1.name += "_"
-                    elif event.key == K_SPACE:
-                        node1.name += " "
-                    elif len(node1.name) < 12:
-                        node1.name += event.unicode
-                if node2.selected:
-                    if not node2.editedname:
-                        node2.name = ""
-                        node2.editedname = True
-                    if event.key == K_DELETE:
-                        node2.name = ""
-                    if event.key == K_BACKSPACE:
-                        node2.name = node2.name[:-1]
-                    elif event.key == K_MINUS:
-                        node2.name += "_"
-                    elif event.key == K_SPACE:
-                        node2.name += " "
-                    elif len(node2.name) < 12:
-                        node2.name += event.unicode
+                for node in nodes:
+                    if node.selected:
+                        if not node.editedname:
+                            node.name = ""
+                            node.editedname = True
+                        if event.key == K_DELETE:
+                            node.name = ""
+                        if event.key == K_BACKSPACE:
+                            node.name = node.name[:-1]
+                        elif event.key == K_MINUS:
+                            node.name += "_"
+                        elif event.key == K_SPACE:
+                            node.name += " "
+                        elif len(node.name) < 12:
+                            node.name += event.unicode
             if event.type == MOUSEBUTTONDOWN:
-                if hover1:
-                    node1.anchor = True
-                    node1.selected = not node1.selected
-                else:
-                    node1.selected = False
-                if hover2:
-                    node2.anchor = True
-                    node2.selected = not node2.selected
-                else:
-                    node2.selected = False
+                for i in range(len(nodes)):
+                    if hover[i]:
+                        nodes[i].anchor = True
+                        nodes[i].selected = not nodes[i].selected
+                    else:
+                        nodes[i].selected = False
             if event.type == MOUSEBUTTONUP:
-                node1.anchor = False
-                node2.anchor = False
-                if node1.outrect[1]:
-                    for rect in node2.inrects:
-                        if rect[0].collidepoint(pygame.mouse.get_pos()):
-                            connections.append([node1.outrect[0], rect[0]])
-                            rect[1] = 1
-                if node2.outrect[1]:
-                    for rect in node1.inrects:
-                        if rect[0].collidepoint(pygame.mouse.get_pos()):
-                            connections.append([node2.outrect[0], rect[0]])
-                            rect[1] = 1
+                for node1 in nodes:
+                    for node2 in nodes:
+                        if node1 != node2:
+                            node1.anchor = False
+                            if node1.outrect[1]:
+                                for rect in node2.inrects:
+                                    if rect[0].collidepoint(pygame.mouse.get_pos()):
+                                        connections.append([node1.outrect[0], rect[0]])
+                                        rect[1] = 1
 
         # Edit the node name
-        if node1.selected:
-            keys_pressed = pygame.key.get_pressed()
-            if keys_pressed[K_BACKSPACE] and len(node1.name) > 0:
-                key_time += 0.1
-                if key_time > 3:
-                    node1.name = node1.name[:-1]
-            else:
-                key_time = 0
-
-        if node2.selected:
-            keys_pressed = pygame.key.get_pressed()
-            if keys_pressed[K_BACKSPACE] and len(node2.name) > 0:
-                key_time += 0.1
-                if key_time > 3:
-                    node2.name = node2.name[:-1]
-            else:
-                key_time = 0
-
-        screen.fill((0, 25, 20))
-
-        # Node follows mouse when selected
-        hover1 = node1.display()
-        hover2 = node2.display()
-        if node1.anchor:
-            node1.rect.centerx, node1.rect.centery = pygame.mouse.get_pos()
-        elif node2.anchor:
-            node2.rect.centerx, node2.rect.centery = pygame.mouse.get_pos()
+        for node in nodes:
+            if node.selected:
+                keys_pressed = pygame.key.get_pressed()
+                if keys_pressed[K_BACKSPACE] and len(node.name) > 0:
+                    key_time += 0.1
+                    if key_time > 3:
+                        node.name = node.name[:-1]
+                else:
+                    key_time = 0
         
         for node in connections:
             pygame.draw.line(screen, (255, 255, 255), node[0].center, node[1].center, 3)
